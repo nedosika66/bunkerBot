925cb2f8f27c76e8e0e10a71cc2a4a3f01df233e
Nazar Nedosika <125128205+nedosika66@users.noreply.github.com>
2025-12-28 01:16:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ TOKEN = "8402946106:AAHFYIWFoCDKWSkOFSBpqN1crVa1fSTvoDY"
 GEMINI_API_KEY = "AIzaSyCZQ_95nAIx9tjziCVW2w1__M0mcDlJgUU"
 
 DB_CONFIG = {
-    "host": "127.0.0.1",
-    "user": "root",
-    "password": "NaZaR123!", # Твій пароль
-    "database": "bunkerBase"
+    'host': 'ballast.proxy.rlwy.net',       # Твій Host з Railway
+    'port': 21007,                          # Твій Port (ЦИФРАМИ)
+    'user': 'root',
+    'password': 'LuELvuFwNdqcSGRrvfwyzShrWJTfMsJu', # Твій Пароль
+    'database': 'railway'
 }
